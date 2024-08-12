0768c2aaa318f7dd00e9cdb0077132c705d4d535
Jzarr-software-house <davidwandavid18@gmail.com>
2024-08-12 18:25:56 +0500

    working

--- app.js
+++ app.js
@@ -26,11 +26,16 @@ cloudinary.config({
 });
 
 const s3Client = new S3Client({
-    region: serverConfig.awsRegion,
+    region: "ap-south-1",
     credentials: {
-        accessKeyId: serverConfig.awsAccessKey,
-        secretAccessKey: serverConfig.awsSecrectAccesskey
+        accessKeyId: 'AKIA3FLDXL2KM3EOC6FF',
+        secretAccessKey: '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'
     }
+    // region: serverConfig.awsRegion,
+    // credentials: {
+    //     accessKeyId: serverConfig.awsAccessKey,
+    //     secretAccessKey: serverConfig.awsSecrectAccesskey
+    // }
 });
 
 // const getObjectURL = async (key) => {
